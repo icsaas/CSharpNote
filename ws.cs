d5f4113d5b51042f6cd10e9582f7cead12ec96bd
matrixorz <matrix.orz@gmail.com>
2016-06-12 23:56:07 +0800

    测试webservice窗口不再本地

---
+++ ws.cs
@@ -0,0 +1,11 @@
+#web.config
+<system.web>
+   <webService>
+     <protocols>
+       <add name="HttpSoap"/>
+       <add name="HttpPost"/>
+       <add name="HttpGet"/>
+       <add name="Documentation"/>
+     </protocols>
+    </webService>
+</system.web>
