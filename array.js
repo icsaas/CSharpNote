d93981b8f3b01927708060c342011ddeb21f3b65
matrixorz <matrix.orz@gmail.com>
2016-08-16 21:31:46 +0800

    add array fix

---
+++ array.js
@@ -0,0 +1,13 @@
+Array.prototype.clone = function() {
+    return [].concat(this);
+};
+
+function mySplit(str, ch) {
+    var pos, start = 0, result = [];
+    while ((pos = str.indexOf(ch, start)) != -1) {
+        result.push(str.substring(start, pos));
+        start = pos + 1;
+    }
+    result.push(str.substr(start));
+    return(result);    
+}
